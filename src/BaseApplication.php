6ee8d883f81b4cbb2281548e0e44cf71a44294b3
stefanorosanelli <stefano.rosanelli@gmail.com>
2018-08-09 12:53:42 +0200

    feat: add `BaseApplication` + test

---
+++ src/BaseApplication.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2018 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\WebTools;
+
+use BEdita\WebTools\Shell\CacheShell;
+use Cake\Error\Middleware\ErrorHandlerMiddleware;
+use Cake\Http\BaseApplication as CakeBaseApplication;
+use Cake\Http\MiddlewareQueue;
+use Cake\Routing\Middleware\AssetMiddleware;
+use Cake\Routing\Middleware\RoutingMiddleware;
+
+/**
+ * Application setup base class.
+ *
+ * This defines the bootstrapping logic and middleware layers you
+ * want to use in your application.
+ */
+class BaseApplication extends CakeBaseApplication
+{
+    /**
+     * {@inheritDoc}
+     *
+     * Replace CakePHP `cache` command with \BEdita\WebTools\Shell\CacheShell
+     */
+    public function console($commands)
+    {
+        return $commands->addMany($commands->autoDiscover())
+            ->add('cache', CacheShell::class);
+    }
+
+    /**
+     * Setup the middleware queue your application will use.
+     *
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
+     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
+     */
+    public function middleware($middlewareQueue) : MiddlewareQueue
+    {
+        $middlewareQueue
+            // Catch any exceptions in the lower layers,
+            // and make an error page/response
+            ->add(ErrorHandlerMiddleware::class)
+
+            // Handle plugin/theme assets like CakePHP normally does.
+            ->add(AssetMiddleware::class)
+
+            // Add routing middleware.
+            ->add(new RoutingMiddleware($this));
+
+        return $middlewareQueue;
+    }
+}
