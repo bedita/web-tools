b93b1c6e86c387f25eb22fc254dabd49ce0b1731
Dante Di Domenico <dante@didoda.com>
2021-04-15 11:53:49 +0200

    chore fix: file php doc

--- src/Controller/Component/ApiFormatterComponent.php
+++ src/Controller/Component/ApiFormatterComponent.php
@@ -2,10 +2,17 @@
 declare(strict_types=1);
 
 /**
- * API Formatter
+ * BEdita, API-first content management framework
+ * Copyright 2021 Atlas Srl, ChannelWeb Srl, Chialab Srl
  *
- * Copyright 2021 Atlas Srl
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
  */
+
 namespace BEdita\WebTools\Controller\Component;
 
 use Cake\Collection\Collection;
