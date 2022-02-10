c2cd8bc54fd73a12edfd10085e47392e6b506de4
batopa <batopa@gmail.com>
2022-02-10 16:06:37 +0100

    test: unit tests for RequestRolesPolicy

---
+++ tests/test_app/TestApp/Policy/InvokeCustomPolicy.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace TestApp\Policy;
+
+use Authorization\IdentityInterface;
+use Cake\Http\ServerRequest;
+
+class InvokeCustomPolicy
+{
+    public function __invoke(?IdentityInterface $identity, ServerRequest $request) {
+        return true;
+    }
+}
