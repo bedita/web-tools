96cbd70773d7adb42eba0575f87a8a2098430e13
batopa <batopa@gmail.com>
2022-02-11 17:37:14 +0100

    test: add tests for identity helper

---
+++ tests/TestCase/View/Helper/IdentityHelperTest.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2022 Atlas Srl, ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\WebTools\Test\TestCase;
+
+use BEdita\WebTools\Identity;
+use BEdita\WebTools\View\Helper\IdentityHelper;
+use Cake\Http\ServerRequest;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+/**
+ * {@see BEdita\WebTools\View\Helper\IdentityHelper} Test Case
+ *
+ * @coversDefaultClass \BEdita\WebTools\View\Helper\IdentityHelper
+ */
+class IdentityHelperTest extends TestCase
+{
+    /**
+     * Keep Identity instance.
+     *
+     * @var \BEdita\WebTools\Identity
+     */
+    protected $identity = null;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $data = [
+            'id' => 1,
+            'roles' => ['admin', 'basic'],
+        ];
+
+        $this->identity = new Identity($data);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->identity = null;
+    }
+
+    /**
+     * Test that delegating a configured method works as expected.
+     *
+     * @return void
+     * @covers ::__call()
+     */
+    public function testDelgateOk(): void
+    {
+        $request = (new ServerRequest())->withAttribute('identity', $this->identity);
+        $View = new View($request);
+        $identityHelper = new IdentityHelper($View);
+
+        static::assertTrue($identityHelper->hasRole('admin'));
+        static::assertFalse($identityHelper->hasRole('manager'));
+    }
+
+    /**
+     * Test that a `BadMethodCallException` is thrown calling method without identity.
+     *
+     * @return void
+     * @covers ::__call()
+     */
+    public function testDelegateNoIdentity(): void
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('Cannot call `hasRole` on stored identity since it is not an object.');
+
+        $request = (new ServerRequest())->withAttribute('identity', null);
+        $View = new View($request);
+        $identityHelper = new IdentityHelper($View);
+        $identityHelper->hasRole('admin');
+    }
+
+    /**
+     * Test that a `BadMethodCallException` is thrown calling not delegated method.
+     *
+     * @return void
+     * @covers ::__call()
+     */
+    public function testDelegateBadDelegateMethod(): void
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('Cannot call `fakeMethod`. Make sure to add it to `delegateMethods`.');
+
+        $request = (new ServerRequest())->withAttribute('identity', $this->identity);
+        $View = new View($request);
+        $identityHelper = new IdentityHelper($View);
+        $identityHelper->fakeMethod();
+    }
+}
