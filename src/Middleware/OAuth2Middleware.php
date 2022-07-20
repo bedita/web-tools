ec6d18e515ec6670114d5bb6a25e24c7d77b3d16
stefanorosanelli <stefano.rosanelli@gmail.com>
2022-07-20 11:14:38 +0200

    feat: oauth2 authentication

---
+++ src/Middleware/OAuth2Middleware.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2022 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\WebTools\Middleware;
+
+use Authentication\Authenticator\Result;
+use Cake\Utility\Hash;
+use Laminas\Diactoros\Response\RedirectResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Middleware to handle OAuth2 flow.
+ */
+class OAuth2Middleware implements MiddlewareInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $result = $request->getAttribute('authenticationResult');
+        if (empty($result) || !$result instanceof Result || !is_array($result->getData())) {
+            return $handler->handle($request);
+        }
+
+        $authUrl = Hash::get($result->getData(), 'authUrl');
+        if (empty($authUrl)) {
+            return $handler->handle($request);
+        }
+
+        return new RedirectResponse($authUrl);
+    }
+}
