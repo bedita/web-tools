76eb669be9ba9d75ba06e3ca1f66236f2dbb5d1c
dante di domenico <dante@didoda.com>
2023-01-17 17:31:57 +0100

    feat: csv trait

---
+++ src/Utility/CsvTrait.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2023 Atlas Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\WebTools\Utility;
+
+
+use Cake\Core\InstanceConfigTrait;
+use Cake\Http\Exception\BadRequestException;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+
+/**
+ * Trait for share Csv stuff.
+ */
+trait CsvTrait
+{
+    use InstanceConfigTrait;
+
+    /**
+     * Progressively read a CSV file, line by line.
+     *
+     * @param string $path Path to CSV file.
+     * @return \Generator<array<string, string>>
+     */
+    public function read($path): \Generator
+    {
+        $fh = fopen($path, 'rb');
+        if (!$fh) {
+            throw new FileNotFoundException(sprintf('Unable to open file in read mode: %s', $path));
+        }
+        $options = $this->getConfig('csv');
+        $delimiter = $options['delimiter'];
+        $enclosure = $options['enclosure'];
+        $escape = $options['escape'];
+        flock($fh, LOCK_SH);
+        $header = fgetcsv($fh, null, $delimiter, $enclosure, $escape);
+        if ($header === false) {
+            throw new BadRequestException(sprintf('Unable to get csv data for file: %s', $path));
+        }
+        $i = 0;
+        while (($row = fgetcsv($fh, null, $delimiter, $enclosure, $escape)) !== false) {
+            yield array_combine($header, $row);
+            $i++;
+        }
+        flock($fh, LOCK_UN);
+        fclose($fh);
+    }
+}
---
+++ tests/TestCase/Utility/CsvTraitTest.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2023 Atlas Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\WebTools\Test\TestCase\Utility;
+
+use BEdita\WebTools\Utility\CsvTrait;
+use Cake\Http\Exception\BadRequestException;
+use Cake\TestSuite\TestCase;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+
+/**
+ * {@see \BEdita\WebTools\Utility\CsvTrait} Test Case
+ *
+ * @coversDefaultClass \BEdita\WebTools\Utility\CsvTrait
+ */
+class CsvTraitTest extends TestCase
+{
+    use CsvTrait;
+
+    /**
+     * Test `read` method, FileNotFoundException
+     *
+     * @return void
+     * @covers ::read()
+     */
+    public function testReadFileNotFound(): void
+    {
+        $path = './file-that-does-not-exist.csv';
+        $expected = new FileNotFoundException(sprintf('Unable to open file in read mode: %s', $path));
+        $this->expectException(get_class($expected));
+        $this->expectExceptionCode($expected->getCode());
+        $this->expectExceptionMessage($expected->getMessage());
+        $this->read($path);
+    }
+
+    /**
+     * Test `read` method, BadRequestException
+     *
+     * @return void
+     * @covers ::read()
+     */
+    public function testReadBadRequest(): void
+    {
+        $path = sprintf('%s/tests/files/bad-file.csv', getcwd());
+        $expected = new BadRequestException(sprintf('Unable to get csv data for file: %s', $path));
+        $this->expectException(get_class($expected));
+        $this->expectExceptionCode($expected->getCode());
+        $this->expectExceptionMessage($expected->getMessage());
+        $this->read($path);
+    }
+
+    /**
+     * Test `read` method
+     *
+     * @return void
+     * @covers ::read()
+     */
+    public function testRead(): void
+    {
+        $path = sprintf('%s/tests/files/test.csv', getcwd());
+        $expected = [
+            ['title' => 'The Great Gatsby', 'author' => 'Francis Scott Fitzgerald'],
+            ['title' => 'Moby-Dick', 'author' => 'Herman Melville'],
+            ['title' => 'Ulysses', 'author' => 'James Joyce'],
+            ['title' => 'Hearth of Darkness', 'author' => 'Joseph Conrad'],
+        ];
+        $actual = [];
+        foreach ($this->read($path) as $row) {
+            $actual[] = $row;
+        }
+        static::assertSame($expected, $actual);
+    }
+}
