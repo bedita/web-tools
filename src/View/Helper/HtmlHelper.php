5cbb016a300713fe839c8c48b02451f9425b278d
dante di domenico <dante@didoda.com>
2024-09-06 14:54:42 +0200

    refactor: HtmlHelper meta functions

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -157,15 +157,11 @@ class HtmlHelper extends CakeHtmlHelper
                 return '';
             }
         }
-        $html = $this->meta([
+
+        return (string)$this->meta([
             'name' => 'author',
             'content' => h($creator),
         ]);
-        if ($html === null) {
-            $html = '';
-        }
-
-        return $html;
     }
 
     /**
@@ -182,15 +178,11 @@ class HtmlHelper extends CakeHtmlHelper
                 return '';
             }
         }
-        $html = $this->meta([
+
+        return (string)$this->meta([
             'http-equiv' => 'Content-Style-Type',
             'content' => 'text/css',
         ]);
-        if ($html === null) {
-            $html = '';
-        }
-
-        return $html;
     }
 
     /**
@@ -211,15 +203,11 @@ class HtmlHelper extends CakeHtmlHelper
         if (!empty($project['version'])) {
             $version = $project['version'];
         }
-        $html = $this->meta([
+
+        return (string)$this->meta([
             'name' => 'generator',
             'content' => trim(sprintf('%s %s', $project['name'], $version)),
         ]);
-        if ($html === null) {
-            $html = '';
-        }
-
-        return $html;
     }
 
     /**
