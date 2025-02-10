80b51a98506378c15e7091b87c459d6f74f72023
dante di domenico <dante@didoda.com>
2025-02-10 18:15:25 +0100

    feat: cleanResponse in formatter

--- src/Controller/Component/ApiFormatterComponent.php
+++ src/Controller/Component/ApiFormatterComponent.php
@@ -169,7 +169,7 @@ class ApiFormatterComponent extends Component
      */
     public function cleanResponse(
         array $response,
-        array $options = ['included', 'links', 'schema', 'relationships']
+        array $options = ['included', 'links', 'schema', 'relationships', 'attributes' => ['extra']]
     ): array {
         return ApiTools::cleanResponse($response, $options);
     }
