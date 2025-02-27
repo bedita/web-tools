8f3e4a084ac71fe8116373a408fc6557fefe4d90
dante di domenico <dante@didoda.com>
2025-02-27 16:20:54 +0100

    fix: paths

--- config/paths.php
+++ config/paths.php
@@ -26,52 +26,6 @@ if (!defined('DS')) {
  */
 
 /*
- * The full path to the directory which holds "src", WITHOUT a trailing DS.
- */
-define('ROOT', dirname(__DIR__));
-
-/*
- * The actual directory name for the application directory. Normally
- * named 'src'.
- */
-define('APP_DIR', 'src');
-
-/*
- * Path to the application's directory.
- */
-define('APP', ROOT . DS . APP_DIR . DS);
-
-/*
- * Path to the config directory.
- */
-define('CONFIG', ROOT . DS . 'config' . DS);
-
-/*
- * Path to the tests directory.
- */
-define('TESTS', ROOT . DS . 'tests' . DS);
-
-/*
- * Path to the temporary files directory.
- */
-define('TMP', ROOT . DS . 'tmp' . DS);
-
-/*
- * Path to the logs directory.
- */
-define('LOGS', ROOT . DS . 'logs' . DS);
-
-/*
- * Path to the cache files directory. It can be shared between hosts in a multi-server setup.
- */
-define('CACHE', TMP . 'cache' . DS);
-
-/*
- * Path to the resources directory.
- */
-define('RESOURCES', ROOT . DS . 'resources' . DS);
-
-/*
  * The absolute path to the "cake" directory, WITHOUT a trailing DS.
  *
  * CakePHP should always be installed with composer, so look there.
